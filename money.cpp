9c38fbb2c38e1d52438d57b3904b39be437932ad
Xyc| Artem K. <zoom.2000v@gmail.com>
2025-04-02 12:17:08 +0300

    I forgor about commiting in process ;-;

---
+++ money.cpp
@@ -0,0 +1,46 @@
+//
+// Created by хус on 26.03.2025.
+//
+#include "money.h"
+#include <cmath>
+
+#include <fstream>
+
+void moneyAdd(MoneyStruct &target, MoneyStruct &source) {
+    if (source.kop >= 100) {
+        source.grn += source.kop / 100;
+        source.kop %= 100;
+    }
+    target.grn += source.grn;
+    target.kop += source.kop;
+    if (target.kop >= 100) {
+        target.grn += target.kop / 100;
+        target.kop %= 100;
+    }
+}
+
+void moneyMultiply(MoneyStruct& p, const int amount){
+    p.grn *= amount;
+    p.kop *= amount;
+}
+
+void moneyRound(MoneyStruct& p) {
+    while (p.kop>100) {
+        p.kop = p.kop - 100;
+        p.grn++;
+    }
+    p.kop = (round(p.kop/10.0)*10);
+}
+
+void readFile(MoneyStruct& target, std::string fileName) {
+    std::string line;
+    int linesCount = 0;
+    std::ifstream file(fileName);
+    if (!file.is_open()) {
+        std::cout << "error occured sorry lol :P";
+    }
+    while (std::getline(file, line)) {
+        linesCount++;
+    }
+}
+
